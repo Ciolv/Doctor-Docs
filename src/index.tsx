217e0f5820fe109e96cb614ba00fcdc576dc151d
Ciolv <73327363+Ciolv@users.noreply.github.com>
2022-10-15 11:15:15 +0200

    fix: Enable bootstrap style sheet

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from "./ts/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 // noinspection NestedFunctionCallJS
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
