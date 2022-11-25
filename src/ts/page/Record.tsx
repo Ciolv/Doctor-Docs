1f6aea871ba87027d8590396bb083ec130797442
Ciolv <73327363+Ciolv@users.noreply.github.com>
2022-11-25 09:10:18 +0100

    fix: Fix linter warnings

--- src/ts/page/Record.tsx
+++ src/ts/page/Record.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { ChangeEvent } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { RecordNav } from "../component/RecordNav";
 import "../../css/Record.scss";
@@ -28,28 +27,26 @@ export class Record extends React.Component<Props, State> {
 
   render() {
     return (
-      <>
-        <Container fluid>
-          <Row>
-            <Col id="navigation-pane" xs={2}>
-              <RecordNav />
-            </Col>
-            <Col id="main-content" xs={10}>
-              <div className={"input-bar"}>
-                <input id="fileInput" type="file" onChange={(e) => this.pFile(e)} hidden />
-                <label id="fileInputLabel" htmlFor="fileInput">
-                  <IoAddCircle />
-                </label>
-              </div>
-              <RecordList />
-            </Col>
-          </Row>
-        </Container>
-      </>
+      <Container fluid>
+        <Row>
+          <Col id="navigation-pane" xs={2}>
+            <RecordNav />
+          </Col>
+          <Col id="main-content" xs={10}>
+            <div className={"input-bar"}>
+              <input id="fileInput" type="file" onChange={this.pFile} hidden />
+              <label id="fileInputLabel" htmlFor="fileInput">
+                <IoAddCircle />
+              </label>
+            </div>
+            <RecordList />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 
-  pFile(e: ChangeEvent) {
+  pFile(e: React.ChangeEvent<HTMLInputElement>) {
     const files = (e.target as EventTarget & HTMLInputElement).files;
     Record.postFile(files);
   }
