09de160c6d2e6b0f20761af0f961154cc164a12b
Ciolv <73327363+Ciolv@users.noreply.github.com>
2022-11-29 16:55:13 +0100

    style: Reformat file

--- src/ts/page/Login.tsx
+++ src/ts/page/Login.tsx
@@ -7,31 +7,28 @@ type Props = {};
 type State = {};
 
 const msalConfig = {
-  auth : {
-    clientId : "49e6fa71-9a2c-465e-a3bc-8ba2f15bad61",
-    redirectUri: "http://localhost:3000/home"
-  }
+  auth: {
+    clientId: "49e6fa71-9a2c-465e-a3bc-8ba2f15bad61",
+    redirectUri: "http://localhost:3000/home",
+  },
 };
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 export class Login extends React.Component<Props, State> {
-  async login_popup(){
+  async login_popup() {
     try {
-      await msalInstance.loginPopup({scopes: ["user.read"]})
-
-    }catch(e){
-      console.log(`Login failed ${e}`)
+      await msalInstance.loginPopup({ scopes: ["user.read"] });
+    } catch (e) {
+      console.log(`Login failed ${e}`);
     }
   }
 
   render() {
     return (
-
-        <div>
-          <button className={"login-button"} onClick={()=>this.login_popup()}></button>
-        </div>
-
+      <div>
+        <button className={"login-button"} onClick={() => this.login_popup()}></button>
+      </div>
     );
   }
 }
