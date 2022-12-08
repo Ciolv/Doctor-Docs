18ba53f8515963c58a448a228f30e7961c027f00
Doozzle <yannicroem@gmail.com>
2022-12-08 15:17:42 +0100

    Optimized Login page

--- src/ts/page/Login.tsx
+++ src/ts/page/Login.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import { AuthenticationResult, PublicClientApplication } from "@azure/msal-browser";
 import { Navigate } from "react-router-dom";
 import { Alert } from "react-bootstrap";
+import "../../css/Login.css"
 import { getTokenResponse, msalConfig } from "../utils/AuthHelper";
-
+import mslogo from "../../img/microsoft.svg";
+import doctor from "../../img/Doctor.svg";
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {
   onLogin: (authToken: AuthenticationResult) => void;
@@ -57,15 +59,31 @@ export class Login extends React.Component<Props, State> {
     ) : this.state.loginError ? (
       <div>
         <Alert className={"alert-danger"}>Your login attempt was unsuccessful. Please try it again.</Alert>
-        <button type={"button"} className={"login-button btn btn-primary"} onClick={() => this.loginPopup()}>
-          Login
-        </button>
+        <div className={"welcome-text-container"}>
+          <h1 className={"main-heading"}>Herzlich Willkommen bei Doctor Docs</h1>
+          <img className={"doctor"} alt={"Doctor Docs Logo"} src={doctor} />
+          <p className={"welcome-text"}>Um sich bei der Anwendung anzumelden und Ihre Reise zum sicheren Speichern von Gesundeheitsdokumenten zu starten, drücken Sie bitte auf die Schaltfläche Login und folgen Sie den Anweisungen um sich mit Ihrem Microsoft-Konto anzumelden.</p>
+        </div>
+        <div className={"login-button-container"}>
+          <button type={"button"} className={"login-button"} onClick={() => this.loginPopup()}>
+            Login mit
+            <img alt={"Microsoft Logo"} className={"microsoft-logo"} src={mslogo} />
+          </button>
+        </div>
       </div>
     ) : (
       <div>
-        <button type={"button"} className={"login-button btn btn-primary"} onClick={() => this.loginPopup()}>
-          Login
-        </button>
+        <div className={"welcome-text-container"}>
+          <h1 className={"main-heading"}>Herzlich Willkommen bei Doctor Docs</h1>
+          <div className={"doctor-container"}><img className={"doctor"} alt={"Doctor Docs Logo"} src={doctor} /></div>
+          <p className={"welcome-text"}>Um sich bei der Anwendung anzumelden und Ihre Reise zum sicheren Speichern von Gesundeheitsdokumenten zu starten, drücken Sie bitte auf die Schaltfläche Login und folgen Sie den Anweisungen um sich mit Ihrem Microsoft-Konto anzumelden.</p>
+        </div>
+        <div className={"login-button-container"}>
+          <button type={"button"} className={"login-button"} onClick={() => this.loginPopup()}>
+            Login mit
+            <img alt={"Microsoft Logo"} className={"microsoft-logo"} src={mslogo} />
+          </button>
+        </div>
       </div>
     );
   }
